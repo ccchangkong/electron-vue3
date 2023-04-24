0394bc5d80737ce2e1425f90be7396490db469f6
ccchangkong <chlo0o@qq.com>
2023-04-24 12:28:27 +0800

    update 调整弹窗到父组件，

--- src/components/SearcherBar.vue
+++ src/components/SearcherBar.vue
@@ -1,33 +1,12 @@
 <script setup>
-import { ref } from 'vue'
-import useWebStroe from '@/stroe/index.js'
-const webStroe = useWebStroe()
-const visible = ref(false);
-const str = ref("https://www.baidu.com");
-let btnAbled = true
+import { ref, inject } from 'vue'
+
+const visible = inject('visible');
 
 const handleClick = () => {
     visible.value = true;
 };
-const handleOk = () => {
-    if (btnAbled) {
-        visible.value = false;
-    }
 
-};
-const handleCancel = () => {
-
-    if (btnAbled) {
-        visible.value = false;
-    }
-}
-const handleAdd = async () => {
-    btnAbled = false
-    const result = await myApi.sendUrl(str.value)
-    webStroe.add(result)
-    btnAbled = true
-    // visible.value = false;
-}
 </script>
 
 <template>
@@ -39,19 +18,6 @@ const handleAdd = async () => {
             </template>
         </a-button>
     </div>
-    <a-modal v-model:visible="visible" @ok="handleOk" @cancel="handleCancel" :mask-closable="false">
-        <template #title>
-            Title
-        </template>
-        <div><a-input :style="{ width: '320px' }" default-value="content" placeholder="Please enter something" allow-clear
-                v-model="str" />
-            <a-button type="primary" @click="handleAdd">
-                <template #icon>
-                    <icon-plus />
-                </template>
-            </a-button>
-        </div>
-    </a-modal>
 </template>
 
 <style scoped lang="scss">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,56 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, provide } from 'vue'
 import SearcherBar from '../components/SearcherBar.vue'
 import List from '../components/List.vue'
+import useWebStroe from '@/stroe/index.js'
+const webStroe = useWebStroe()
+const visible = ref(false);
+provide('visible', visible)
+const str = ref("https://www.baidu.com");
 
-defineProps({
-  msg: String,
-})
+let btnAbled = true
 
-const count = ref(0)
+const handleOk = () => {
+  if (btnAbled) {
+    visible.value = false;
+  }
+
+};
+const handleCancel = () => {
+
+  if (btnAbled) {
+    visible.value = false;
+  }
+}
+const handleAdd = async () => {
+  btnAbled = false
+  const result = await myApi.sendUrl(str.value)
+  webStroe.add(result)
+  btnAbled = true
+  // visible.value = false;
+}
+
+
 </script>
 
 <template>
-<main>
-    <SearcherBar/>
-    <List/>
-</main>
+  <main>
+    <SearcherBar />
+    <List />
+    <a-modal v-model:visible="visible" @ok="handleOk" @cancel="handleCancel" :mask-closable="false">
+      <template #title>
+        Title
+      </template>
+      <div><a-input :style="{ width: '320px' }" default-value="content" placeholder="Please enter something" allow-clear
+          v-model="str" />
+        <a-button type="primary" @click="handleAdd">
+          <template #icon>
+            <icon-plus />
+          </template>
+        </a-button>
+      </div>
+    </a-modal>
+  </main>
 </template>
 
 <style scoped>
